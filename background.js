b9e299fcadeb0be7c7acf56f8e16bd5a06327620
Liqiq <illa77mucolajovuch@gmail.com>
2025-02-27 20:15:25 +0200

    cleared background.js from testing code

--- background.js
+++ background.js
@@ -1,17 +1,5 @@
 "use strict";
 
-// chrome.tabs.onCreated.addListener(() => {
-//     console.log("New Tab~");
-    
-// });
-
-// chrome.action.onClicked.addListener((tab) => {
-//     chrome.scripting.executeScript({
-//       target: { tabId: tab.id },
-//       files: ["script.js"]
-//     });
-//   });
-
 chrome.tabs.onActivated.addListener((activeInfo) => {
     chrome.scripting.executeScript({
         target: { tabId: activeInfo.tabId },
